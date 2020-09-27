51adf79e13a9a7d3052c97f4e43ba023e4520ccf
Tron <crazynuggets@outlook.com>
2020-09-28 00:58:36 +0300

    Updated

--- test.sh
+++ test.sh
@@ -12,18 +12,14 @@ echo ""
 echo "==> Installing Server"
 echo "======================="
 echo ""
-npm install --silent --no-warnings
 printf "\033[33m"
-ncu -u --silent
 printf "\033[33m"
-npm update --silent
 printf "\033[33m"
 
 echo
-echo "==> Launching Server"
+echo "==^> Launching Server"
 echo "======================="
 echo
-npm run start --silent
 export launcherCommand=altv-server
 for a in ('dir "./resources" /b /a:d ^| findstr "[^[$]]"') do (
     export "launcherCommand=$launcherCommand --extra-res-folder resources/$a"
