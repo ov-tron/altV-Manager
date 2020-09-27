2aab8f48727ad3210eb6620e3918181a1c257e1e
Tron <crazynuggets@outlook.com>
2020-09-28 02:28:02 +0300

    Updated

--- server-launcher.sh
+++ server-launcher.sh
@@ -10,6 +10,7 @@ echo  "* DOC: 25/09/2020 (OvileAmriam)"
 echo  "* Desc: Automated Server Installer/Updater/Launcher"
 echo "***************************************************************"
 echo ""
+
 echo ""
 echo "==> Installing Server"
 echo "======================="
@@ -20,13 +21,14 @@ ncu -u --silent
 printf "\033[33m"
 npm update --silent
 printf "\033[33m"
+
 echo ""
 echo "==> Launching Server"
 echo "======================="
 echo ""
 npm run start --silent
 export launcherCommand=altv-server
-for a in ('dir "./resources" /b /a:d ^| findstr "[^[$]]"') do (
-    export "launcherCommand=$launcherCommand --extra-res-folder resources/$a"
-)
+for a in resources/*
+    do export "launcherCommand=$launcherCommand --extra-res-folder $a"
+done
 $launcherCommand
