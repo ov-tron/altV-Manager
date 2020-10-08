2f09831fb776cf5460807eecedba0ba3b85a140f
Tron <crazynuggets@outlook.com>
2020-10-08 19:59:04 +0300

    Updated server installer

--- server-manager/server-installer.js
+++ server-manager/server-installer.js
@@ -37,37 +37,39 @@ async function onInstallServer() {
     serverLogger.displayServerLog("\n* Desc: Automated Server Installer/Updater/Launcher")
     serverLogger.displayServerLog("\n***************************************************************")
 
-    const serverUpdateData = await serverUpdater.getServerUpdateData()
-    if (serverUpdateData && !serverUpdateData[0]) {
-        serverLogger.displayServerLog("\n\n\n==> Downloading Server Assets")
-        serverLogger.displayServerLog("\n==============================")
-        await new Promise(async (resolve, reject) => {
-            for (var assetIndex = 0; assetIndex < serverAssets.length; assetIndex++) {
-                serverLogger.displayServerLog("\n\n==> " + serverAssets[assetIndex][serverPlatform] + " [Downloading]")
-                await download(serverAssets[assetIndex][serverPlatform], serverAssets[assetIndex].filePath)
-                    .then(() => {
-                        process.stdout.clearLine()
-                        process.stdout.cursorTo(0)
-                        serverLogger.displayServerLog("==> " + serverAssets[assetIndex][serverPlatform] + " [Downloaded]")
+    await serverUpdater.getServerUpdateData()
+        .then(async (serverUpdateData) => {
+            if (serverUpdateData && !serverUpdateData[0]) {
+                serverLogger.displayServerLog("\n\n\n==> Downloading Server Assets")
+                serverLogger.displayServerLog("\n==============================")
+                await new Promise(async (resolve, reject) => {
+                    for (var assetIndex = 0; assetIndex < serverAssets.length; assetIndex++) {
+                        serverLogger.displayServerLog("\n\n==> " + serverAssets[assetIndex][serverPlatform] + " [Downloading]")
+                        await download(serverAssets[assetIndex][serverPlatform], serverAssets[assetIndex].filePath)
+                            .then(() => {
+                                process.stdout.clearLine()
+                                process.stdout.cursorTo(0)
+                                serverLogger.displayServerLog("==> " + serverAssets[assetIndex][serverPlatform] + " [Downloaded]")
+                            })
+                            .catch(() => {
+                                process.stdout.clearLine()
+                                process.stdout.cursorTo(0)
+                                serverLogger.displayServerLog("==> " + serverAssets[assetIndex][serverPlatform] + " [Failed]")
+                            })
+                    }
+                    resolve()
+                })
+                serverLogger.displayServerLog("\n")
+                await new Promise((resolve, reject) => {
+                    fs.writeFile(serverLauncherData[0], serverLauncherData[1] + "\n" + serverLauncherData[2], () => {
+                        resolve()
                     })
-                    .catch(() => {
-                        process.stdout.clearLine()
-                        process.stdout.cursorTo(0)
-                        serverLogger.displayServerLog("==> " + serverAssets[assetIndex][serverPlatform] + " [Failed]")
-                    })
+                })
+                serverLogger.displayServerLog("\n\n==> Server successfully installed!")
+            } else {
+                serverLogger.displayServerLog("\n\n\n==> Server already upto date!")
             }
-            resolve()
         })
-        serverLogger.displayServerLog("\n")
-        await new Promise((resolve, reject) => {
-            fs.writeFile(serverLauncherData[0], serverLauncherData[1] + "\n" + serverLauncherData[2], () => {
-                resolve()
-            })
-        })
-        serverLogger.displayServerLog("\n\n==> Server successfully installed!")
-    } else {
-        serverLogger.displayServerLog("\n\n\n==> Server already upto date!")
-    }
     serverLogger.displayServerLog("\n\n==> Hint: Use server-launcher to launch your server!\n")
     process.exit(0)
 
